7e7a0a4042a18394f607d798f2b7a8f6b8c5d600
rossputin <rossajmcd@gmail.com>
2018-01-23 15:51:56 +0000

    provide environ dependency

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Serve things"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [environ "1.1.0"]]
   :aot :all
   :main thingserver.core)
