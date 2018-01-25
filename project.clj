2933aef0072013865bd086ee15bab188c97034e9
nick.featch@gmail.com <nick.featch@gmail.com>
2018-01-25 23:15:02 +0000

    add .doc handler

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring/ring-core "1.6.3"]
                  [ring/ring-jetty-adapter "1.6.3"]
-                 [environ "1.1.0"]]
+                 [environ "1.1.0"]
+                 [markdown-clj "1.0.2"]]
   :aot :all
   :main thingserver.core)
--- src/thingserver/core.clj
+++ src/thingserver/core.clj
@@ -1,14 +1,22 @@
 (ns thingserver.core
-  (:use [ring.adapter.jetty]
-        [ring.middleware.resource]
-        [ring.middleware.content-type]
-        [ring.middleware.not-modified]
-        [ring.middleware.file])
-  (:require [environ.core :refer [env]])
- (:gen-class))
+  (:require [clojure.string :as s])
+  (:use [clojure.java.io :only [file]]
+        [ring.middleware.content-type :only [wrap-content-type]]
+        [ring.middleware.not-modified :only [wrap-not-modified]]
+        [ring.adapter.jetty :only [run-jetty]]
+        [ring.middleware.file :only [wrap-file]]
+        [markdown.core :only [md-to-html-string]]
+        [environ.core :only [env]])
+  (:gen-class))
 
-(defn handler [request]
-  {:status 404 :headers {"Content-Type" "text/html"} :body "Not Found"})
+(defn handler [{:keys [uri] :as request}]
+  (let [f1 (file (str (env :dahl-asset-dir) (s/replace uri #".doc" ".html")))
+        f2 (file (str (env :dahl-asset-dir) (s/replace uri #".doc" ".md")))
+        body (cond
+               (and (s/ends-with? uri ".doc") (.exists f1)) (slurp f1)
+               (and (s/ends-with? uri ".doc") (.exists f2)) (md-to-html-string (slurp f2))
+               :else                                        "Not Found")]
+    {:status 200 :headers {"Content-Type" "text/html"} :body body}))
 
 (def app
   (-> (wrap-file handler (env :dahl-asset-dir))
